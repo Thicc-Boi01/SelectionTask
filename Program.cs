fdb37bfaa8e35a0777225d5fd1ab7fdb68bebf50
Thomas-Casey <102041956@student.swin.edu.au>
2021-03-18 15:42:12 +1100

    this sommot is the selection task

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SelectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Please enter your user name");
+
+            string username = Console.ReadLine();
+
+            if (username == "Hard")
+            {
+                Console.WriteLine("Please enter your password");
+
+                string password = Console.ReadLine();
+
+                if (password == "Copy")
+                {
+                    Console.WriteLine("Logging in...");
+                    Console.WriteLine("Welcome Tom");
+                }
+                else
+                {
+                    Console.WriteLine("Incorect Password");
+                }
+            }
+            else {
+                Console.WriteLine("User Does Not Exist");
+            }
+        }
+    }
+}
